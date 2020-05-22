6f9320100d2c7b6e21061dff22550110e77480a8
AhmetNSHN <64427438+AhmetNSHN@users.noreply.github.com>
2020-05-22 21:34:48 +0300

    Update main.java

--- main.java
+++ main.java
@@ -1,18 +1,44 @@
 package online_learing_system;
 import java.awt.BorderLayout;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
 
 import javax.swing.JFrame;
+
+
 public class main {
 	public static void main(String[] args)
 	{
 		JFrame Application = new JFrame();
-		Application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+		Application.setLayout(new GridBagLayout());
+		GridBagConstraints constraints = new GridBagConstraints();
+		constraints.fill = GridBagConstraints.BOTH;
+				
 		menu menu_bar = new menu();
-		white_board whiteboard = new white_board();
-		Application.add(menu_bar,BorderLayout.NORTH);
-		Application.add(whiteboard);
-		Application.setSize(900,1300);
+		Application.setJMenuBar(menu_bar);
+		
+        white_board p = new white_board();		
+		constraints.weightx = 1.0;
+		constraints.weighty = 1.0;		
+		constraints.gridx = 0;
+		constraints.gridy = 0;       
+        Application.add(p, constraints);
+				
+		chat_box chatbox = new chat_box();
+		constraints.weightx = 1;
+		constraints.weighty = 0.2;		
+		constraints.gridx = 0;
+		++constraints.gridy;       
+        Application.add(chatbox, constraints);
+        
+        Application.setSize(900,1000);
 		Application.setVisible(true);
+		Application.setDefaultCloseOperation(JFrame.EXIT_ON_CLOSE);
+		
+        chatbox.connect();
+		
+		
 	}
 
 }
