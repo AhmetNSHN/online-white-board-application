30490519f7a35dd9db94b2411f6d3d221632061e
AhmetNSHN <64427438+AhmetNSHN@users.noreply.github.com>
2020-05-23 18:37:22 +0300

    Update student_white_board.java

--- student_white_board.java
+++ student_white_board.java
@@ -3,12 +3,10 @@ import java.awt.GridLayout;
 
 import javax.swing.*;
 public class student_white_board extends JPanel{
-	public final JButton hand_button;
+	
 	public student_white_board()
 	{
-		hand_button = new JButton("Rise Hand");
-
-		add(hand_button);		
+			
 	}
 
 }
