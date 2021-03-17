3799d52c8fa98f7d76c3a01582675f3bd8b4ae71
AhmetNSHN <64427438+AhmetNSHN@users.noreply.github.com>
2021-03-17 21:32:36 +0400

    Add files via upload

---
+++ online learning system/src/online_learning_system/chat_box.java
@@ -0,0 +1,235 @@
+package online_learning_system;
+
+import java.awt.BorderLayout;
+import java.awt.Color;
+import java.awt.GridBagConstraints;
+import java.awt.GridBagLayout;
+import java.awt.GridLayout;
+import java.awt.event.ActionEvent;
+import java.awt.event.ActionListener;
+import java.io.EOFException;
+import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
+import java.io.ObjectInputStream;
+import java.io.ObjectOutputStream;
+import java.net.ServerSocket;
+import java.net.Socket;
+import java.net.SocketException;
+import java.util.Scanner;
+import javax.swing.BorderFactory;
+import javax.swing.JButton;
+import javax.swing.JOptionPane;
+import javax.swing.JPanel;
+import javax.swing.JScrollPane;
+import javax.swing.JTextArea;
+import javax.swing.JTextField;
+import javax.swing.SwingUtilities;
+
+public class chat_box extends JPanel {
+    BorderLayout border_layout;
+    GridLayout grid_layout;
+    JTextArea message_area;
+    JTextField message_box;
+    JButton send;
+    JButton Attendance;
+
+    private ObjectOutputStream oos;
+    private ObjectInputStream ois;
+    private ServerSocket socket;
+    private Socket connection;
+
+    public chat_box() {
+        setLayout(new GridBagLayout());
+        GridBagConstraints constraints = new GridBagConstraints();
+        constraints.fill = GridBagConstraints.BOTH;
+        setBorder(BorderFactory.createLineBorder(Color.BLACK));
+
+        message_area = new JTextArea();
+        message_area.setEditable(false);
+        message_area.setLineWrap(true);
+        message_area.setWrapStyleWord(true);
+        add(new JScrollPane(message_area));
+        JScrollPane scrollpane = new JScrollPane(message_area);
+        constraints.gridwidth = 3;
+        constraints.weightx = 1.0;
+        constraints.weighty = 1.0;
+        constraints.gridx = 0;
+        constraints.gridy = 0;
+        add(scrollpane, constraints);
+
+        message_box = new JTextField();
+        constraints.gridwidth = 1;
+        constraints.weighty = 0.05;
+        constraints.gridx = 0;
+        constraints.gridy = 1;
+        add(message_box, constraints);
+
+        send = new JButton("Send");
+        setButtonEnabled(false);
+        constraints.weightx = 0.05;
+        constraints.gridx = 1;
+        add(send, constraints);
+        send.addActionListener(new ActionListener() {
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                send(message_box.getText());
+                message_box.setText(" ");
+            }
+        });
+        
+        JButton button1 = new JButton();
+        Attendance = new JButton("Attendance");
+        constraints.weightx = 0.05;
+        constraints.gridx = 2;
+        add(Attendance, constraints);
+        Attendance.addActionListener(new ActionListener() {
+            String student_name;
+
+            @Override
+            public void actionPerformed(ActionEvent e) {
+                try {
+                    File myObj = new File("ATTENDANCE.txt");
+                    Scanner myReader = new Scanner(myObj);
+                    while (myReader.hasNextLine()) {
+                        student_name = myReader.nextLine();
+                        System.out.println(student_name);
+                    }
+                    myReader.close();
+                } catch (FileNotFoundException event) {
+                    System.out.println("An error occurred.");
+                    event.printStackTrace();
+                }
+                JOptionPane.showMessageDialog(null, student_name, "ATTENDANCE", JOptionPane.INFORMATION_MESSAGE);
+            }
+        });
+
+    }
+
+
+    public void connect() {
+        try {
+            socket = new ServerSocket(5000, 100);
+            while (true) {
+                try {
+                    connection = socket.accept();
+                    add_message("SYSTEM: student connected");
+                    send.setEnabled(true);
+                    stream();
+
+
+                    try {
+                        String message = " ";
+                        message = (String) ois.readObject();
+                        try {
+                            File myObj = new File("ATTENDANCE.txt");
+                            if (myObj.createNewFile()) {
+                                System.out.println("File created: " + myObj.getName());
+                            }
+                        } catch (IOException e) {
+                            System.out.println("An error occurred.");
+                            e.printStackTrace();
+                        }
+                        try {
+                            FileWriter myWriter = new FileWriter("ATTENDANCE.txt");
+                            myWriter.write(message);
+                            myWriter.close();
+
+                        } catch (IOException e) {
+                            System.out.println("Cannot write to file.");
+                            e.printStackTrace();
+                        }
+
+                    } catch (ClassNotFoundException e) {
+                        add_message("Can't take the attendance");
+                    }
+
+
+                    processconnection();
+                } catch (EOFException e) {
+                    add_message("SYSTEM: No student left");
+                    setButtonEnabled(false);
+                } finally {
+                    oos.close();
+                    ois.close();
+                    connection.close();
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void stream() throws IOException {
+        oos = new ObjectOutputStream(connection.getOutputStream());
+        oos.flush();
+        ois = new ObjectInputStream(connection.getInputStream());
+    }
+
+
+    private void processconnection() throws IOException {
+        setButtonEnabled(true);
+        String message = " ";
+        while (true) {
+            try {
+                message = (String) ois.readObject();
+
+                if (message.equals("NOTIFICATION")) {
+                    JOptionPane.showMessageDialog(null, "Student Raising Hand", "NOTIFICATION", JOptionPane.WARNING_MESSAGE);
+                } else {
+                    add_message(message);
+                }
+
+            } catch (ClassNotFoundException e) {
+                e.printStackTrace();
+            } catch (StringIndexOutOfBoundsException e) {
+                e.printStackTrace();
+            } catch (SocketException e) {
+                System.out.println("Connection Lost!");
+                break;
+            }
+
+        }
+    }
+
+
+    private void send(String message) {
+        try {
+            oos.writeObject("Teacher:" + message);
+            oos.flush();
+            add_message("Teacher:" + message);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+
+    private void add_message(final String message) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                message_area.append("\n" + message);
+
+            }
+
+        });
+
+    }
+
+
+    public void setButtonEnabled(final boolean state) {
+        SwingUtilities.invokeLater(new Runnable() {
+
+            @Override
+            public void run() {
+                send.setEnabled(state);
+
+            }
+
+        });
+    }
+
+}
---
+++ online learning system/src/online_learning_system/white_board.java
@@ -0,0 +1,318 @@
+package online_learning_system;
+
+import java.awt.*;
+import java.awt.event.MouseAdapter;
+import java.io.*;
+import java.net.Socket;
+import java.rmi.UnknownHostException;
+import java.util.ArrayList;
+import java.awt.event.MouseEvent;
+import java.awt.event.MouseMotionAdapter;
+
+
+import javax.swing.*;
+
+import online_learning_system.menu.status;
+
+
+public class white_board extends JPanel implements Runnable {
+    // constructor with port
+    private Socket socket = new Socket("", 5001);
+    private DataInputStream input = null;
+    private DataOutputStream out = null;
+
+
+    private final ArrayList<Point> line_coordinates = new ArrayList<>();
+    private final ArrayList<Point> circle_coordinates = new ArrayList<>();
+    private final ArrayList<Point> rectangle_coordinates = new ArrayList<>();
+    private final ArrayList<Point> line_start = new ArrayList<>();
+    private final ArrayList<Point> circle_start = new ArrayList<>();
+    private final ArrayList<Point> rectangle_start = new ArrayList<>();
+
+    private final ArrayList<Point> circle_Pen = new ArrayList<>();
+    private final ArrayList<Point> square_Pen = new ArrayList<>();
+    private final ArrayList<Point> eraser_Pen = new ArrayList<>();
+
+    int beginX_Line = 0;
+    int endX_Line = 0;
+    int endY_Line = 0;
+    int beginY_Line = 0;
+    int beginX_Circle = 0;
+    int endX_Circle = 0;
+    int endY_Circle = 0;
+    int beginY_Circle = 0;
+
+    int beginX_Rectangle = 0;
+    int endX_Rectangle = 0;
+    int endY_Rectangle = 0;
+    int beginY_Rectangle = 0;
+
+    public ArrayList<String> outputToSend = new ArrayList<>();
+
+    public white_board() throws IOException {
+        menu g = new menu();
+        addMouseListener(
+                new MouseAdapter() {
+                    public void mousePressed(MouseEvent event) {
+                        if (g.getdraw_status() == status.line) {
+                            beginX_Line = endX_Line = event.getX();
+                            beginY_Line = endY_Line = event.getY();
+                            Point begin = new Point();
+                            begin.x = beginX_Line;
+                            begin.y = beginY_Line;
+                            line_start.add(begin);
+                            outputToSend.removeAll(outputToSend);
+                            outputToSend.add("DrawLine");
+                            outputToSend.add(""+beginX_Line);
+                            outputToSend.add(""+beginY_Line);
+                            repaint();
+                        }
+
+                        if (g.getdraw_status() == status.circle) {
+                            beginX_Circle = endX_Circle = event.getX();
+                            beginY_Circle = endY_Circle = event.getY();
+                            Point begin = new Point();
+                            begin.x = beginX_Circle;
+                            begin.y = beginY_Circle;
+                            circle_start.add(begin);
+                            outputToSend.removeAll(outputToSend);
+                            outputToSend.add("DrawCircle");
+                            outputToSend.add(""+beginX_Circle);
+                            outputToSend.add(""+beginY_Circle);
+                            repaint();
+                        }
+
+                        if (g.getdraw_status() == status.rectangle) {
+                            beginX_Rectangle = endX_Rectangle = event.getX();
+                            beginY_Rectangle = endY_Rectangle = event.getY();
+                            Point begin = new Point();
+                            begin.x = beginX_Rectangle;
+                            begin.y = beginY_Rectangle;
+                            rectangle_start.add(begin);
+                            outputToSend.removeAll(outputToSend);
+                            outputToSend.add("DrawRectangle");
+                            outputToSend.add(""+beginX_Rectangle);
+                            outputToSend.add(""+beginY_Rectangle);
+                            repaint();
+                        }
+                    }
+
+                    public void mouseReleased(MouseEvent event) {
+                        if (g.getdraw_status() == status.line) {
+                            endX_Line = event.getX();
+                            endY_Line = event.getY();
+                            Point end = new Point();
+                            end.x = endX_Line;
+                            end.y = endY_Line;
+                            line_coordinates.add(end);
+                            outputToSend.add(""+endX_Line);
+                            outputToSend.add(""+endY_Line);
+                            try {
+                                this.send(outputToSend);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            repaint();
+                        }
+
+                        if (g.getdraw_status() == status.circle) {
+                            endX_Circle = event.getX();
+                            endY_Circle = event.getY();
+                            Point end = new Point();
+                            end.x = endX_Circle;
+                            end.y = endY_Circle;
+                            circle_coordinates.add(end);
+                            outputToSend.add(""+endX_Circle);
+                            outputToSend.add(""+endY_Circle);
+                            try {
+                                this.send(outputToSend);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            repaint();
+                        }
+
+                        if (g.getdraw_status() == status.rectangle) {
+                            endX_Rectangle = event.getX();
+                            endY_Rectangle = event.getY();
+                            Point end = new Point();
+                            end.x = endX_Rectangle;
+                            end.y = endY_Rectangle;
+                            rectangle_coordinates.add(end);
+                            outputToSend.add(""+endX_Rectangle);
+                            outputToSend.add(""+endY_Rectangle);
+                            try {
+                                this.send(outputToSend);
+                            } catch (IOException e) {
+                                e.printStackTrace();
+                            }
+                            repaint();
+                        }
+                    }
+
+                    public void send(ArrayList<String> outputToSend) throws IOException {
+                            System.out.println("Connected");
+                            OutputStream outputStream = socket.getOutputStream();
+                            ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                            objectOutputStream.writeObject(outputToSend);
+
+                        return;
+
+                    }
+
+                });
+        addMouseMotionListener(
+                new MouseMotionAdapter() {
+
+                    @Override
+                    public void mouseDragged(MouseEvent e) {
+                        if (g.getdraw_status() == status.line) {
+                            endX_Line = e.getX();
+                            endY_Line = e.getY();
+                            repaint();
+                        }
+                        if (g.getdraw_status() == status.circle) {
+                            endX_Circle = e.getX();
+                            endY_Circle = e.getY();
+                            repaint();
+                        }
+
+                        if (g.getdraw_status() == status.rectangle) {
+                            endX_Rectangle = e.getX();
+                            endY_Rectangle = e.getY();
+                            repaint();
+                        }
+
+                        if(g.getdraw_status() == status.circlePen)
+                        {
+                            outputToSend.add("CirclePen");
+                            circle_Pen.add(e.getPoint());
+                            outputToSend.add(""+ e.getX());
+                            outputToSend.add(""+e.getY());
+                            try {
+                                this.send(outputToSend);
+                            } catch (IOException i) {
+                                i.printStackTrace();
+                            }
+                            outputToSend.removeAll(outputToSend);
+                            repaint();
+                        }
+
+                        if(g.getdraw_status() == status.squarePen)
+                        {
+                            outputToSend.add("SquarePen");
+                            square_Pen.add(e.getPoint());
+                            outputToSend.add(""+ e.getX());
+                            outputToSend.add(""+e.getY());
+                            try {
+                                this.send(outputToSend);
+                            } catch (IOException i) {
+                                i.printStackTrace();
+                            }
+                            outputToSend.removeAll(outputToSend);
+                            repaint();
+                        }
+
+
+//                        if(g.getdraw_status() == status.eraserPen)
+//                        {
+////                            line_coordinates.removeAll(line_coordinates);
+////                            circle_coordinates.removeAll(circle_coordinates);
+////                            rectangle_coordinates.removeAll(rectangle_coordinates);
+////                            line_start.removeAll(line_start);
+////                            circle_start.removeAll(line_start);
+////                            rectangle_start.removeAll(rectangle_start);
+////
+////                            circle_Pen.removeAll(circle_Pen);
+////                            square_Pen.removeAll(square_Pen);
+////                            repaint();
+//                            //eraser();
+//                            repaint();
+//                        }
+
+                    }
+
+                    public void send(ArrayList<String> outputToSend) throws IOException {
+                        System.out.println("Connected");
+                        OutputStream outputStream = socket.getOutputStream();
+                        ObjectOutputStream objectOutputStream = new ObjectOutputStream(outputStream);
+                        objectOutputStream.writeObject(outputToSend);
+
+                        return;
+                    }
+
+
+                });
+
+    }
+
+    @Override
+    public void paintComponent(Graphics g) {
+        super.paintComponent(g);
+
+        //LINE DRAWING
+        for (int i = 0; i < line_coordinates.size(); i++) {
+            g.drawLine(line_start.get(i).x, line_start.get(i).y, line_coordinates.get(i).x, line_coordinates.get(i).y);
+        }
+        g.drawLine(beginX_Line, beginY_Line, endX_Line, endY_Line);
+
+
+        // CIRCLE DRAWING
+        if (beginX_Circle > endX_Circle && beginY_Circle > endY_Circle)
+            g.drawOval(endX_Circle, endY_Circle, beginX_Circle - endX_Circle, beginY_Circle - endY_Circle);
+        if (beginX_Circle > endX_Circle && beginY_Circle < endY_Circle)
+            g.drawOval(endX_Circle, beginY_Circle, beginX_Circle - endX_Circle, endY_Circle - beginY_Circle);
+        if (beginX_Circle < endX_Circle && beginY_Circle > endY_Circle)
+            g.drawOval(beginX_Circle, endY_Circle, endX_Circle - beginX_Circle, beginY_Circle - endY_Circle);
+        else
+            g.drawOval(beginX_Circle, beginY_Circle, endX_Circle - beginX_Circle, endY_Circle - beginY_Circle);
+        for (int i = 0; i < circle_coordinates.size(); i++) {
+            if (circle_start.get(i).x > circle_coordinates.get(i).x && circle_start.get(i).y > circle_coordinates.get(i).y)
+                g.drawOval(circle_coordinates.get(i).x, circle_coordinates.get(i).y, circle_start.get(i).x - circle_coordinates.get(i).x, circle_start.get(i).y - circle_coordinates.get(i).y);
+            if (circle_start.get(i).x > circle_coordinates.get(i).x && circle_start.get(i).y < circle_coordinates.get(i).y)
+                g.drawOval(circle_coordinates.get(i).x, circle_start.get(i).y, circle_start.get(i).x - circle_coordinates.get(i).x, circle_coordinates.get(i).y - circle_start.get(i).y);
+            if (circle_start.get(i).x < circle_coordinates.get(i).x && circle_start.get(i).y > circle_coordinates.get(i).y)
+                g.drawOval(circle_start.get(i).x, circle_coordinates.get(i).y, circle_coordinates.get(i).x - beginX_Circle, circle_start.get(i).y - circle_coordinates.get(i).y);
+            else
+                g.drawOval(circle_start.get(i).x, circle_start.get(i).y, circle_coordinates.get(i).x - circle_start.get(i).x, circle_coordinates.get(i).y - circle_start.get(i).y);
+        }
+
+
+        // RECTANGLE DRAWING
+        if (beginX_Rectangle > endX_Rectangle && beginY_Rectangle > endY_Rectangle)
+            g.drawRect(endX_Rectangle, endY_Rectangle, beginX_Rectangle - endX_Rectangle, beginY_Rectangle - endY_Rectangle);
+        if (beginX_Rectangle > endX_Rectangle && beginY_Rectangle < endY_Rectangle)
+            g.drawRect(endX_Rectangle, beginY_Rectangle, beginX_Rectangle - endX_Rectangle, endY_Rectangle - beginY_Rectangle);
+        if (beginX_Rectangle < endX_Rectangle && beginY_Rectangle > endY_Rectangle)
+            g.drawRect(beginX_Rectangle, endY_Rectangle, endX_Rectangle - beginX_Rectangle, beginY_Rectangle - endY_Rectangle);
+        else
+            g.drawRect(beginX_Rectangle, beginY_Rectangle, endX_Rectangle - beginX_Rectangle, endY_Rectangle - beginY_Rectangle);
+        for (int i = 0; i < rectangle_coordinates.size(); i++) {
+            if (rectangle_start.get(i).x > rectangle_coordinates.get(i).x && rectangle_start.get(i).y > rectangle_coordinates.get(i).y)
+                g.drawRect(rectangle_coordinates.get(i).x, rectangle_coordinates.get(i).y, rectangle_start.get(i).x - rectangle_coordinates.get(i).x, rectangle_start.get(i).y - rectangle_coordinates.get(i).y);
+            if (rectangle_start.get(i).x > rectangle_coordinates.get(i).x && rectangle_start.get(i).y < rectangle_coordinates.get(i).y)
+                g.drawRect(rectangle_coordinates.get(i).x, rectangle_start.get(i).y, rectangle_start.get(i).x - rectangle_coordinates.get(i).x, rectangle_coordinates.get(i).y - rectangle_start.get(i).y);
+            if (rectangle_start.get(i).x < rectangle_coordinates.get(i).x && rectangle_start.get(i).y > rectangle_coordinates.get(i).y)
+                g.drawRect(rectangle_start.get(i).x, rectangle_coordinates.get(i).y, rectangle_coordinates.get(i).x - beginX_Rectangle, rectangle_start.get(i).y - rectangle_coordinates.get(i).y);
+            else
+                g.drawRect(rectangle_start.get(i).x, rectangle_start.get(i).y, rectangle_coordinates.get(i).x - rectangle_start.get(i).x, rectangle_coordinates.get(i).y - rectangle_start.get(i).y);
+        }
+
+        for(Point point : circle_Pen)
+        {
+            g.fillOval(point.x, point.y, 100, 100);
+        }
+        for(Point point : square_Pen)
+        {
+            g.fillRect(point.x, point.y, 100, 100);
+        }
+
+        //System.out.println(outputToSend);
+    }
+
+
+    @Override
+    public void run() {
+        white_board runner = this;
+    }
+}
